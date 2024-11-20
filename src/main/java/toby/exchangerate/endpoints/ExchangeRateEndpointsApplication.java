3411c97d7e05f9fe2a1ff9b00bfdd86ab1d1a25d
Larvact <tobyg7794@hotmail.com>
2024-11-20 19:03:36 +0000

    Initial microservice commit

---
+++ src/main/java/toby/exchangerate/endpoints/ExchangeRateEndpointsApplication.java
@@ -0,0 +1,13 @@
+package toby.exchangerate.endpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateEndpointsApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ExchangeRateEndpointsApplication.class, args);
+    }
+}
