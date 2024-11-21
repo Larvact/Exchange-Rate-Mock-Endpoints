9c1733198e60fa390bd3e5ffb7d063a55b5083c8
Larvact <tobyg7794@hotmail.com>
2024-11-21 19:06:15 +0000

    Changed the latest to a post request checking the base currency. Added exception handler to return the correct error code should it be malformed

--- src/main/java/toby/exchangerate/endpoints/ExchangeRateApiResource.java
+++ src/main/java/toby/exchangerate/endpoints/ExchangeRateApiResource.java
@@ -1,15 +1,18 @@
 package toby.exchangerate.endpoints;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import toby.exchangerate.ResourceReader;
 import toby.exchangerate.ExchangeRateEndpointsApplication;
+import toby.exchangerate.common.exception.api.ApiMalformedRequestException;
 import toby.exchangerate.json.JsonHandler;
-import toby.exchangerate.json.api.exchangerates.LatestCurrencyExchangeRatesResponse;
+import toby.exchangerate.json.api.exchangerates.latest.LatestCurrencyExchangeRatesRequest;
+import toby.exchangerate.json.api.exchangerates.latest.LatestCurrencyExchangeRatesResponse;
 
 import java.io.IOException;
+
+import static java.lang.StringTemplate.STR;
+import static toby.exchangerate.common.data.DataTransformationUtils.orElse;
 
 @RestController
 @RequestMapping(path = "v1")
@@ -19,9 +22,21 @@ public class ExchangeRateApiResource
     private static final String USD_LATEST_RESPONSE_PATH = "/mockresponse/exchangerateapi/latest/usdResponse.json";
     private final ResourceReader resourceReader;
 
-    @GetMapping(path = "/latest")
-    public LatestCurrencyExchangeRatesResponse getLatestCurrencyExchangeRate() throws IOException
+    @PostMapping(path = "/latest")
+    public LatestCurrencyExchangeRatesResponse getLatestCurrencyExchangeRate(@RequestBody final LatestCurrencyExchangeRatesRequest latestCurrencyExchangeRatesRequest) throws IOException
     {
-        return JsonHandler.deserializeFromJson(resourceReader.readResource(USD_LATEST_RESPONSE_PATH, ExchangeRateEndpointsApplication.class), LatestCurrencyExchangeRatesResponse.class);
+        if(!orElse(latestCurrencyExchangeRatesRequest, request -> orElse(request.getBaseCurrencySymbol(), symbol -> !symbol.isBlank(), false), false))
+        {
+            throw new ApiMalformedRequestException("Base currency symbol cannot be empty");
+        }
+        final var baseCurrencySymbol = latestCurrencyExchangeRatesRequest.getBaseCurrencySymbol();
+        if("USD".equals(baseCurrencySymbol))
+        {
+            return JsonHandler.deserializeFromJson(resourceReader.readResource(USD_LATEST_RESPONSE_PATH, ExchangeRateEndpointsApplication.class), LatestCurrencyExchangeRatesResponse.class);
+        }
+        else
+        {
+            throw new ApiMalformedRequestException(STR."Base currency symbol [\{baseCurrencySymbol}] is not implemented in the mock data control flow. Implement it or change the request.");
+        }
     }
 }
--- src/main/java/toby/exchangerate/exception/ApiExceptionHandler.java
+++ src/main/java/toby/exchangerate/exception/ApiExceptionHandler.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import toby.exchangerate.common.exception.ApiResponseException;
+import toby.exchangerate.common.exception.api.ApiMalformedRequestException;
+import toby.exchangerate.common.exception.api.ApiResponseException;
 
 import java.io.IOException;
 import java.time.Instant;
@@ -17,6 +18,14 @@ public class ApiExceptionHandler
     {
         final var internalServerError = HttpStatus.INTERNAL_SERVER_ERROR;
         final var responseException = new ApiResponseException(exception.getMessage(), exception, internalServerError, Instant.now());
+        return new ResponseEntity<>(responseException, internalServerError);
+    }
+
+    @ExceptionHandler(value = ApiMalformedRequestException.class)
+    public ResponseEntity<Object> handleBadRequestException(final Exception badRequestException)
+    {
+        final var internalServerError = HttpStatus.BAD_REQUEST;
+        final var responseException = new ApiResponseException(badRequestException.getMessage(), badRequestException, internalServerError, Instant.now());
         return new ResponseEntity<>(responseException, internalServerError);
     }
 }
